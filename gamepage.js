91f1850142d8ae8f11aa81ad5171394881ddcdc5
ramprakashkushwah <102748887+ramprakashkushwah@users.noreply.github.com>
2022-09-27 19:47:15 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,32 @@
+// random value generated
+  var y= Math.floor(Math.random() * 10+ 1);
+// counting the number of guesses
+// made for correct Guess
+var x = document.getElementById("gueesField").value;
+  
+if(x==y)
+{
+    alert("CONGRATULATION!!"+playername+"YOU GUESSED IT RIGHT IN "
+    +guess+"GUESS");
+guess=1;
+}
+// function for number guessed by user     
+else if (x > y)
+
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER ");
+
+}
+else 
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER ")
+}
+function playAgain(){
+y = Math.floor(Math.raandom()* 10 + 1);
+}
+
+function submit(){
+  submit=document.getElementById("submit").value;
+}
---
+++ login.js
@@ -0,0 +1,12 @@
+// Create login Function here.
+function Login(){
+    player_name = document.getElementById("playername").value;
+localStorage.setItem("player_name", player_name);
+window.location = "gamepage.html";
+
+}
+function logout(){
+    plaayer5_name =  document.getElementBtId("p")
+}
+
+
